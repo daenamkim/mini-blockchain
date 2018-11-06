3f9f2e2f09acaba5fb292ba497d1ea2759ad1f49
Daenam Kim <daenam.kim@gmail.com>
2018-11-07 06:56:27 +0900

    Add isChainValid method.

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ class Block {
   }
 
   calculateHash() {
-    return SHA256(this.index + this.previousHash + this.timestamp, JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
   }
 }
 
@@ -32,9 +32,36 @@ class Blockchain {
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
+
+  isChainValid() {
+    // Skip for the genesis block.
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 let daenamCoin = new Blockchain();
 daenamCoin.addBlock(new Block(1, "06/11/2018", {amount: 4}));
 daenamCoin.addBlock(new Block(2, "07/11/2018", {amount: 10}));
+// Show the chain.
 console.log(JSON.stringify(daenamCoin, null, 4));
+console.log('Is blockchain valid? ' + daenamCoin.isChainValid());
+// Current hash will be broken.
+daenamCoin.chain[1].data = { amount: 100 };
+console.log('Is blockchain valid? ' + daenamCoin.isChainValid());
+
+// Relationship with a previous block will be broken.
+daenamCoin.chain[1].hash = daenamCoin.chain[1].calculateHash();
+console.log('Is blockchain valid? ' + daenamCoin.isChainValid());
